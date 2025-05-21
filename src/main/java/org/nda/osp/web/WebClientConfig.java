336cfd004df79c47ca61d87b4113665434f2d3f8
dnikiforov1967 <dnikiforov@mail.ru>
2025-05-21 21:30:17 +0300

    Fix

--- src/main/java/org/nda/osp/web/WebClientConfig.java
+++ src/main/java/org/nda/osp/web/WebClientConfig.java
@@ -2,6 +2,8 @@ package org.nda.osp.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
